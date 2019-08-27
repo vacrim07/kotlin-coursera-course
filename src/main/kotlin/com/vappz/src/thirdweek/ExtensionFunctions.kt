3eef54d5257fa98d77c57758696cf7ceb03c112a
Vladimir Vasquez <vacrim07@gmail.com>
2019-08-26 22:32:40 -0500

    Extension functions

---
+++ src/main/kotlin/com/vappz/src/thirdweek/ExtensionFunctions.kt
@@ -0,0 +1,9 @@
+package com.vappz.src.thirdweek
+
+class ExtensionFunctions {
+
+}
+fun main(){
+    val c: Char = "abc".lastChar()
+    println(c)
+}
---
+++ src/main/kotlin/com/vappz/src/thirdweek/Utils.kt
@@ -0,0 +1,7 @@
+package com.vappz.src.thirdweek
+
+// Long assignment
+//fun String.lastChar() = this.get(this.length - 1)
+
+// Short assignment
+fun String.lastChar() = get(length -1)
